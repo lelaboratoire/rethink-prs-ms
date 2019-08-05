4609c3fdfc7d8e341ef0f856af7218fc8ba917ef
Trang Le <grixor@gmail.com>
2019-08-05 20:31:28 +0700

    use bioinformatics citation style

--- build/build.sh
+++ build/build.sh
@@ -14,7 +14,7 @@ manubot process \
   --log-level=INFO
 
 # pandoc settings
-CSL_PATH=build/assets/style.csl
+CSL_PATH=build/assets/bioinformatics.csl
 BIBLIOGRAPHY_PATH=output/references.json
 INPUT_PATH=output/manuscript.md
 
